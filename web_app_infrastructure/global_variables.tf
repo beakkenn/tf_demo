3ffe25e4b277fc3657625e4420dd13f8a4a2e59b
Kenneth Beak <kennethbeakcoretek@Kenneths-MBP.attlocal.net>
2024-01-31 07:39:25 -0500

    removing dev from rg name

--- web_app_infrastructure/global_variables.tf
+++ web_app_infrastructure/global_variables.tf
@@ -1,6 +1,6 @@
 locals {
   resource_group_webapp      = var.create_resource_group ? azurerm_resource_group.network[0] : data.azurerm_resource_group.network[0]
-  resource_group_webapp_name = "rg-${var.global_settings.workload}-${var.global_settings.environment}-${var.global_settings.location_short}-01"
+  resource_group_webapp_name = "rg-${var.global_settings.workload}-${var.global_settings.location_short}-01"
 }
 
 
