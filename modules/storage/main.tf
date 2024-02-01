1ae46ddb6c4d8ced4a40497bcb5e255e041d1eb1
Kenneth Beak <kennethbeakcoretek@Kenneths-MBP.attlocal.net>
2024-01-31 20:17:25 -0500

    adding storage module

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -7,11 +7,17 @@ variable "storage_account" {
     account_replication_type = string
     tags                     = map(string)
     containers               = list(object({
-      name                   = string
-      container_access_type  = string
+      name                 = string
+      container_access_type = string
+    }))
+    private_endpoints = list(object({
+      name                           = string
+      subnet_id                      = string
+      private_service_connection_name = string
     }))
   })
 }
+
 
 variable "resource_group_name" {
   description = "Name of the resource group"
@@ -34,6 +40,22 @@ resource "azurerm_storage_container" "container" {
   storage_account_name = azurerm_storage_account.storage_account.name
   container_access_type = each.value.container_access_type
 }
+
+resource "azurerm_private_endpoint" "pep" {
+  for_each             = { for pep in var.storage_account.pep : pep.name => pep }
+  name                 = each.value.name
+  location             = azurerm_storage_account.storage_account.location
+  resource_group_name  = azurerm_storage_account.storage_account.resource_group_name
+  subnet_id            = each.value.subnet_id
+
+  private_service_connection {
+    name                           = each.value.private_service_connection_name
+    private_connection_resource_id = azurerm_storage_account.storage_account.id
+    is_manual_connection           = false
+    subresource_names              = ["blob"]
+  }
+}
+
 
 output "storage_account_id" {
   value = azurerm_storage_account.storage_account.id
